97009230e27be447164d56441292baff57f8fe3f
TakuyaBetchaku <Takuya.betchaku@sony.com>
2025-05-16 16:36:25 +0900

    クラスを関数のように使用する例

--- オブジェクト指向/class6.py
+++ オブジェクト指向/class6.py
@@ -4,3 +4,12 @@ class Pico1:
 
 p1 = Pico1()
 print(str(p1))
+
+class Pico2:
+    def __call__(self,p,a):
+        return p*5 + a*3
+p2 = Pico2()
+print(p2("りんご","パイナップル")) #p2を関数のように扱える
+
+test = "Hello!!"
+print(test*2)
