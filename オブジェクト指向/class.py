e31e87543780fbd7e319764bbe3fa3a1909401b6
TakuyaBetchaku <Takuya.betchaku@sony.com>
2025-05-16 16:00:35 +0900

    クラスのselfについて学習した

--- オブジェクト指向/class.py
+++ オブジェクト指向/class.py
@@ -4,3 +4,24 @@ class Pico:#クラス　設計図のようなもの
 
 P = Pico() #クラスからインスタンスを生成、pがインスタンス
 P.sayPPAP() #メソッドの呼び出し
+
+class Greeting1:
+    def sayHello(self):#メソッド
+        print("hello!")
+
+
+gr_1 = Greeting1() #インスタンスの生成
+gr_1.sayHello() #メソッドの呼び出し 
+
+print("\n")
+
+class Greeting2:
+    def sayHello(self):#メソッド
+        print("hello!")
+    
+    def sayNice(self):
+        self.sayHello() #selfはインスタンス自身　インスタンスから他のメソッドを呼び出す
+        print("Nice to meet you!")
+
+gr_2 = Greeting2()
+gr_2.sayNice()
